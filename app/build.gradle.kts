b492b76ed4eb12038bf7b3f1c6ecca9c7108e086
nyanishinde <nyanishinde10@gmail.com>
2024-05-19 13:34:18 +0530

    Completed the backend code for database connection and to store data to firebase realtime storage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+
+
 }
--- app/src/main/java/com/example/realtimedb/MainActivity.java
+++ app/src/main/java/com/example/realtimedb/MainActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -44,11 +49,18 @@ public class MainActivity extends AppCompatActivity {
 
                 UserDM user = new UserDM(name,email,phone,desc);
 
-                myRootReference.child("Users").child(user.getUserName()).setValue(user);
-
+                myRootReference.child("Users").child(user.getUserName()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(MainActivity.this, "Added", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
-
-
     }
 }
